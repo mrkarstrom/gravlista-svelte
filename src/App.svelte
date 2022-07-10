2fef08bb9e84a4883572c3fbd0cc1454e76ca776
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-07-10 16:35:52 +0200

    Adding input-form

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,24 @@
 <script>
-import data from './assets/gravlista.json' 
+  import data from './assets/gravlista.json';
 
-let list = data.data
+  let list = data.data;
 </script>
 
 <main>
- 
+  <h1>Add Post</h1>
+  <div class="form-control">
+    <label for="post" class="label">Name</label>
+    <input type="text" name="author" class="input" placeholder="username" />
+    <label for="post" class="label">Post</label>
+    <textarea
+      name="post"
+      id=""
+      cols="30"
+      rows="10"
+      placeholder="Your text here"
+    />
+  </div>
 </main>
 
 <style>
- 
 </style>
